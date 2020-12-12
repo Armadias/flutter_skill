2ebc3c13e943b40c278a87df5a3759bf3d2b452d
Armadias <follietmartin@gmail.com>
2020-12-12 14:33:12 +0100

    liste de cours et de compétences, v1

--- lib/Ecran/DrawerFile/EcranCoursEleve.dart
+++ lib/Ecran/DrawerFile/EcranCoursEleve.dart
@@ -19,11 +19,81 @@ class EcranCoursEleve extends StatefulWidget {
 
 class EcranCoursEleveEtat extends State<EcranCoursEleve> 
 {
+  bool isChecked =false;
+  List<dynamic> listeTest = ["francis", "jean", "papa", "nana", "titi", "toto"];
+  List<bool> listeTestBool = [false, true, false, false, false, true];
 
   @override 
   Widget build(BuildContext context)
-  {
+  { 
     return Scaffold(
-      );
+      appBar: AppBar(
+        title: Text(
+          'Liste de vos cours',
+          style: kLabelStyle,
+        ),
+      ),
+      drawer: CustomDrawer(
+        profil: widget.profil,
+        statusString: widget.status,
+        isInListe: true,
+        ),
+        body: Stack(
+          children: <Widget>[
+            colorGradient,
+            constructeurListeCours(),
+        ],
+      ) 
+    );
   }   
+
+  Widget constructeurListeCours()
+  {
+    return new ListView.builder(
+      itemCount: listeTest.length,
+      itemBuilder: (context, index)
+      {
+        return new ExpansionTile(
+          title: new Text(
+            listeTest[index],
+            style: kLabelStyle,
+          ),
+          children: <Widget>[
+            constructeurListeCompetences()
+          ],
+        );
+      }
+    );
+  }
+
+  Widget constructeurListeCompetences()
+  {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: listeTest.length,
+        itemBuilder: (context, index) {
+          return Container(
+            color: listeTestBool[index]? Colors.green : null,
+          child : CheckboxListTile(
+            value: listeTestBool[index], 
+            onChanged: (val)
+            {
+              if (val)
+              {
+                listeTestBool[index] = true;
+              }
+              else
+                listeTestBool[index] = false;
+                setState(() {
+                });
+            },
+            title: new Text(
+              listeTest[index]
+            ),
+          )
+          );
+        },
+      );
+         
+  }
 }
